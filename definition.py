e1e6c726ad35572a513ee49d5161e5db5a03dd04
kazuhiko1979 <challengevolley1979@gmail.com>
2023-11-15 10:51:08 +0900

    completed section 39

--- definition.py
+++ definition.py
@@ -1,6 +1,12 @@
+import pandas as pd
+
 class Definition:
 
 	def __init__(self, term):
 		self.term = term
 	def get(self):
-		pass
+		df = pd.read_csv("data.csv")
+		return tuple(df.loc[df['word'] == self.term]['definition'])
+
+d = Definition(term="sun")
+print(d.get())
